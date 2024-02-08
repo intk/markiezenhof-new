c2fac181f36ca06232e484a8b0b382780af69fe4
Rane Hyvönen <hyvonenrane@gmail.com>
2024-02-08 12:53:52 +0100

    fix(ui): add markiezenhof subscribe url

--- frontend/src/components/theme/Footer/NewsletterSubscribe.jsx
+++ frontend/src/components/theme/Footer/NewsletterSubscribe.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { defineMessages, injectIntl } from 'react-intl';
 
 const messages = defineMessages({
@@ -11,6 +10,10 @@ const messages = defineMessages({
 const NewsletterSubscribe = ({ intl }) => (
   <div className="newsletter-subscribe">
     <a
+      href={
+        'https://gemeente-bergen-op-zoom-markiezenhof.email-provider.eu/' +
+        'memberforms/subscribe/standalone/form/?a=kvlm5pg896&l=ccfiafesre'
+      }
       target="_blank"
       rel="noopener noreferrer"
       className="newsletter-subscribe-link"
